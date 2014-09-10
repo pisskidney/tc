285b88fdd2e52f4cdb97a160844ec3d5786d4c21
Peter Horvath <horvthpeter@gmail.com>
2014-09-10 17:11:48 +0000

    199_2 500

--- 199_2/all.py
+++ 199_2/all.py
@@ -16,4 +16,20 @@ class StringMult(object):
         return res
 
 
-print StringMult().times("Racecar", -5)
+class TriangleCount(object):
+    def g(self, i, j):
+        return j - i + 1
+
+    def count(self, n):
+        c = 0
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                g1 = self.g(i, j)
+                g2 = self.g(i, j - i)
+                g1 = g1 if g1 >= 0 else 0
+                g2 = g2 if g2 >= 0 else 0
+                c += g1 + g2
+        return c
+
+
+print TriangleCount().count(4)
