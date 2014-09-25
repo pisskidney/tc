c66461d0038414c4321ce9a0b377f8bc9cbd5320
Peter Horvath <horvthpeter@gmail.com>
2014-09-25 14:04:51 +0000

    502_2 500

--- 502_2/all.py
+++ 502_2/all.py
@@ -16,3 +16,23 @@ class TheProgrammingContestDivTwo(object):
                 return s, sum(pen[:-1])
 
         return s, sum(pen)
+
+
+class TheLotteryBothDivs(object):
+    def find(self, sufs):
+        sufs = list(set(sufs))
+        s = sufs[:]
+        for i in range(len(sufs)):
+            for j in range(i + 1, len(sufs)):
+                if sufs[i].endswith(sufs[j]) and sufs[i] in s:
+                    s.remove(sufs[i])
+                elif sufs[j].endswith(sufs[i]) and sufs[j] in s:
+                    s.remove(sufs[j])
+        c = 0
+        for x in s:
+            c += 1.0 / (10**len(x))
+        return c
+
+
+#print TheLotteryBothDivs().find(("8542861", "1954", "6", "523", "000000000", "5426", "8"))
+print TheLotteryBothDivs().find(("47", "58", "4747", "502"))
