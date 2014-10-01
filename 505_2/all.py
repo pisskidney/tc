16124b94d13d841a2819eb3590f9fd5b9b36d65a
Peter Horvath <horvthpeter@gmail.com>
2014-10-01 00:50:39 +0000

    505_2 500

--- 505_2/all.py
+++ 505_2/all.py
@@ -8,3 +8,27 @@ class SentenceCapitalizerInator(object):
             if p[i] == '.' and i != len(p) - 1:
                 p = p[:i+2] + p[i+2].upper() + p[i + 3:]
         return p
+
+
+class PerfectSequences(object):
+    def fixIt(self, s):
+        if len(s) == 1:
+            return 'Yes'
+        for i in xrange(len(s)):
+            p = 1
+            for j in xrange(len(s)):
+                if i != j and p < 10**12:
+                    p *= s[j]
+            q = 0
+            for j in xrange(len(s)):
+                if i != j:
+                    q += s[j]
+
+            ans = -1
+            if p == 0 and q == 0:
+                ans = 0
+            elif (p - 1) != 0:
+                ans = float(q)/(p - 1)
+            if ans >= 0 and int(ans) == ans and ans != s[i]:
+                return 'Yes'
+        return 'No'
