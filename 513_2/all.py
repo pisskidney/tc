87856208b3a2763654bc6a3656ba4e4882c3ac78
Peter Horvath <horvthpeter@gmail.com>
2014-10-29 23:01:32 +0000

    513_2 500 fast

--- 513_2/all.py
+++ 513_2/all.py
@@ -16,9 +16,10 @@ class TrainingCamp():
                     if p[j][kk] == 'X' and a[i][kk] == '-':
                         good = False
                 comp += 'X' if good else '-'
-            
+
             res.append(comp)
         return res
+
 
 class YetAnotherIncredibleMachine():
     def isok(self, left, right, ball):
@@ -30,22 +31,21 @@ class YetAnotherIncredibleMachine():
         res = 1
         for i in xrange(len(mounts)):
             subres = 0
-            for j in xrange(mounts[i] - lens[i], mounts[i] + 1):
-                ok = True
-                for k in balls:
-                    if not self.isok(j, j + lens[i], k):
-                        ok = False
-                        break
-                if ok:
-                    subres += 1
+            pmin = mounts[i] - lens[i]
+            pmax = mounts[i] + lens[i]
+            for j in balls:
+                if j <= mounts[i] and j >= pmin:
+                    pmin = j + 1
+                if j >= mounts[i] and j <= pmax:
+                    pmax = j - 1
+            if pmin >= pmax or pmax - pmin + 1 < lens[i]:
+                subres = 0
+            else:
+                subres = pmax - pmin - lens[i] + 1
+
             res *= subres
             res %= 1000000009
         return res
 
 
-print YetAnotherIncredibleMachine().countWays([0]*50, [10000]*50, range(1, 51))
-print YetAnotherIncredibleMachine().countWays((0,), (1,), (0,))
-
-
-
-
+print YetAnotherIncredibleMachine().countWays((7,), (10,), (3, 4,))
