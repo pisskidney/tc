39fdc37d74ade6ecb5de0393ecf0d302404a9ef0
Peter Horvath <horvthpeter@gmail.com>
2014-10-29 22:38:17 +0000

    513_2 500 slowish

--- 513_2/all.py
+++ 513_2/all.py
@@ -20,3 +20,32 @@ class TrainingCamp():
             res.append(comp)
         return res
 
+class YetAnotherIncredibleMachine():
+    def isok(self, left, right, ball):
+        if left <= ball and right >= ball:
+            return False
+        return True
+
+    def countWays(self, mounts, lens, balls):
+        res = 1
+        for i in xrange(len(mounts)):
+            subres = 0
+            for j in xrange(mounts[i] - lens[i], mounts[i] + 1):
+                ok = True
+                for k in balls:
+                    if not self.isok(j, j + lens[i], k):
+                        ok = False
+                        break
+                if ok:
+                    subres += 1
+            res *= subres
+            res %= 1000000009
+        return res
+
+
+print YetAnotherIncredibleMachine().countWays([0]*50, [10000]*50, range(1, 51))
+print YetAnotherIncredibleMachine().countWays((0,), (1,), (0,))
+
+
+
+
